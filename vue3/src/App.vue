73e7991ba1db298a05cef1bd3fc0383c4547df69
Dzw <2649212120@qq.com>
2025-02-06 16:51:36 +0800

    up:"初始化项目"

---
+++ vue3/src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>App</div>
+</template>
+
+<script >
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "App",
+});
+</script>
+<script setup>
+import { ref, reactive, onMounted } from "vue";
+</script>
+
+<style scoped lang="scss" ></style>
---
+++ 文档/vue.md
@@ -0,0 +1,14 @@
+### VUE
+
+##### 一，安装Vue3
+
+```vue
+命令 pnpm create vue@latest
+```
+
+##### 二，
+
+```
+
+```
+
