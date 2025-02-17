f4f0580d063a213c369e36d03581a0a077e09c72
Dzw <2649212120@qq.com>
2025-02-17 16:05:13 +0800

    up:"性能优化"

--- Webpack/courseware/webpack_docs/docs/.vuepress/.temp/internal/clientAppSetups.js
+++ Webpack/courseware/webpack_docs/docs/.vuepress/.temp/internal/clientAppSetups.js
@@ -1,7 +1,7 @@
-import clientAppSetup0 from 'D:/item/2025study/Vue/Webpack/courseware/webpack_docs/node_modules/@vuepress/plugin-active-header-links/lib/client/clientAppSetup.js'
-import clientAppSetup1 from 'D:/item/2025study/Vue/Webpack/courseware/webpack_docs/node_modules/@vuepress/plugin-nprogress/lib/client/clientAppSetup.js'
-import clientAppSetup2 from 'D:/item/2025study/Vue/Webpack/courseware/webpack_docs/node_modules/@vuepress/theme-default/lib/client/clientAppSetup.js'
-import clientAppSetup3 from 'D:/item/2025study/Vue/Webpack/courseware/webpack_docs/node_modules/vuepress-plugin-copy-code2/lib/client/appSetup.js'
+import clientAppSetup0 from 'D:/study/2025/Vue/Webpack/courseware/webpack_docs/node_modules/@vuepress/plugin-active-header-links/lib/client/clientAppSetup.js'
+import clientAppSetup1 from 'D:/study/2025/Vue/Webpack/courseware/webpack_docs/node_modules/@vuepress/plugin-nprogress/lib/client/clientAppSetup.js'
+import clientAppSetup2 from 'D:/study/2025/Vue/Webpack/courseware/webpack_docs/node_modules/@vuepress/theme-default/lib/client/clientAppSetup.js'
+import clientAppSetup3 from 'D:/study/2025/Vue/Webpack/courseware/webpack_docs/node_modules/vuepress-plugin-copy-code2/lib/client/appSetup.js'
 
 export const clientAppSetups = [
   clientAppSetup0,
--- Webpack/courseware/webpack_docs/docs/.vuepress/.temp/internal/pagesComponents.js
+++ Webpack/courseware/webpack_docs/docs/.vuepress/.temp/internal/pagesComponents.js
@@ -2,79 +2,79 @@ import { defineAsyncComponent } from 'vue'
 
 export const pagesComponents = {
   // path: /
-  "v-8daa1a0e": defineAsyncComponent(() => import(/* webpackChunkName: "v-8daa1a0e" */"D:/item/2025study/Vue/Webpack/courseware/webpack_docs/docs/.vuepress/.temp/pages/index.html.vue")),
-  // path: /base/base.html
-  "v-a6cc4bba": defineAsyncComponent(() => import(/* webpackChunkName: "v-a6cc4bba" */"D:/item/2025study/Vue/Webpack/courseware/webpack_docs/docs/.vuepress/.temp/pages/base/base.html.vue")),
-  // path: /base/clean.html
-  "v-21f1ba15": defineAsyncComponent(() => import(/* webpackChunkName: "v-21f1ba15" */"D:/item/2025study/Vue/Webpack/courseware/webpack_docs/docs/.vuepress/.temp/pages/base/clean.html.vue")),
-  // path: /base/config.html
-  "v-e703ecdc": defineAsyncComponent(() => import(/* webpackChunkName: "v-e703ecdc" */"D:/item/2025study/Vue/Webpack/courseware/webpack_docs/docs/.vuepress/.temp/pages/base/config.html.vue")),
-  // path: /base/css.html
-  "v-80c5304a": defineAsyncComponent(() => import(/* webpackChunkName: "v-80c5304a" */"D:/item/2025study/Vue/Webpack/courseware/webpack_docs/docs/.vuepress/.temp/pages/base/css.html.vue")),
-  // path: /base/development.html
-  "v-34029aa3": defineAsyncComponent(() => import(/* webpackChunkName: "v-34029aa3" */"D:/item/2025study/Vue/Webpack/courseware/webpack_docs/docs/.vuepress/.temp/pages/base/development.html.vue")),
-  // path: /base/font.html
-  "v-85d4d936": defineAsyncComponent(() => import(/* webpackChunkName: "v-85d4d936" */"D:/item/2025study/Vue/Webpack/courseware/webpack_docs/docs/.vuepress/.temp/pages/base/font.html.vue")),
-  // path: /base/html.html
-  "v-ab115cee": defineAsyncComponent(() => import(/* webpackChunkName: "v-ab115cee" */"D:/item/2025study/Vue/Webpack/courseware/webpack_docs/docs/.vuepress/.temp/pages/base/html.html.vue")),
-  // path: /base/image.html
-  "v-03183b7a": defineAsyncComponent(() => import(/* webpackChunkName: "v-03183b7a" */"D:/item/2025study/Vue/Webpack/courseware/webpack_docs/docs/.vuepress/.temp/pages/base/image.html.vue")),
-  // path: /base/javascript.html
-  "v-0bd200c7": defineAsyncComponent(() => import(/* webpackChunkName: "v-0bd200c7" */"D:/item/2025study/Vue/Webpack/courseware/webpack_docs/docs/.vuepress/.temp/pages/base/javascript.html.vue")),
-  // path: /base/minifyHtml.html
-  "v-50d92e02": defineAsyncComponent(() => import(/* webpackChunkName: "v-50d92e02" */"D:/item/2025study/Vue/Webpack/courseware/webpack_docs/docs/.vuepress/.temp/pages/base/minifyHtml.html.vue")),
-  // path: /base/optimizeCss.html
-  "v-65f7fcd8": defineAsyncComponent(() => import(/* webpackChunkName: "v-65f7fcd8" */"D:/item/2025study/Vue/Webpack/courseware/webpack_docs/docs/.vuepress/.temp/pages/base/optimizeCss.html.vue")),
-  // path: /base/other.html
-  "v-499655ae": defineAsyncComponent(() => import(/* webpackChunkName: "v-499655ae" */"D:/item/2025study/Vue/Webpack/courseware/webpack_docs/docs/.vuepress/.temp/pages/base/other.html.vue")),
-  // path: /base/output.html
-  "v-5a4f3333": defineAsyncComponent(() => import(/* webpackChunkName: "v-5a4f3333" */"D:/item/2025study/Vue/Webpack/courseware/webpack_docs/docs/.vuepress/.temp/pages/base/output.html.vue")),
-  // path: /base/production.html
-  "v-6940ce0a": defineAsyncComponent(() => import(/* webpackChunkName: "v-6940ce0a" */"D:/item/2025study/Vue/Webpack/courseware/webpack_docs/docs/.vuepress/.temp/pages/base/production.html.vue")),
-  // path: /base/
-  "v-1455d425": defineAsyncComponent(() => import(/* webpackChunkName: "v-1455d425" */"D:/item/2025study/Vue/Webpack/courseware/webpack_docs/docs/.vuepress/.temp/pages/base/index.html.vue")),
-  // path: /base/server.html
-  "v-5e3bb631": defineAsyncComponent(() => import(/* webpackChunkName: "v-5e3bb631" */"D:/item/2025study/Vue/Webpack/courseware/webpack_docs/docs/.vuepress/.temp/pages/base/server.html.vue")),
-  // path: /base/summary.html
-  "v-ce2194d0": defineAsyncComponent(() => import(/* webpackChunkName: "v-ce2194d0" */"D:/item/2025study/Vue/Webpack/courseware/webpack_docs/docs/.vuepress/.temp/pages/base/summary.html.vue")),
+  "v-8daa1a0e": defineAsyncComponent(() => import(/* webpackChunkName: "v-8daa1a0e" */"D:/study/2025/Vue/Webpack/courseware/webpack_docs/docs/.vuepress/.temp/pages/index.html.vue")),
   // path: /intro/asset.html
-  "v-0670fc65": defineAsyncComponent(() => import(/* webpackChunkName: "v-0670fc65" */"D:/item/2025study/Vue/Webpack/courseware/webpack_docs/docs/.vuepress/.temp/pages/intro/asset.html.vue")),
+  "v-0670fc65": defineAsyncComponent(() => import(/* webpackChunkName: "v-0670fc65" */"D:/study/2025/Vue/Webpack/courseware/webpack_docs/docs/.vuepress/.temp/pages/intro/asset.html.vue")),
   // path: /intro/group.html
-  "v-581e5b94": defineAsyncComponent(() => import(/* webpackChunkName: "v-581e5b94" */"D:/item/2025study/Vue/Webpack/courseware/webpack_docs/docs/.vuepress/.temp/pages/intro/group.html.vue")),
+  "v-581e5b94": defineAsyncComponent(() => import(/* webpackChunkName: "v-581e5b94" */"D:/study/2025/Vue/Webpack/courseware/webpack_docs/docs/.vuepress/.temp/pages/intro/group.html.vue")),
   // path: /intro/learn.html
-  "v-7d0ce4de": defineAsyncComponent(() => import(/* webpackChunkName: "v-7d0ce4de" */"D:/item/2025study/Vue/Webpack/courseware/webpack_docs/docs/.vuepress/.temp/pages/intro/learn.html.vue")),
+  "v-7d0ce4de": defineAsyncComponent(() => import(/* webpackChunkName: "v-7d0ce4de" */"D:/study/2025/Vue/Webpack/courseware/webpack_docs/docs/.vuepress/.temp/pages/intro/learn.html.vue")),
   // path: /intro/pre.html
-  "v-10b408dc": defineAsyncComponent(() => import(/* webpackChunkName: "v-10b408dc" */"D:/item/2025study/Vue/Webpack/courseware/webpack_docs/docs/.vuepress/.temp/pages/intro/pre.html.vue")),
+  "v-10b408dc": defineAsyncComponent(() => import(/* webpackChunkName: "v-10b408dc" */"D:/study/2025/Vue/Webpack/courseware/webpack_docs/docs/.vuepress/.temp/pages/intro/pre.html.vue")),
   // path: /intro/
-  "v-f9e30908": defineAsyncComponent(() => import(/* webpackChunkName: "v-f9e30908" */"D:/item/2025study/Vue/Webpack/courseware/webpack_docs/docs/.vuepress/.temp/pages/intro/index.html.vue")),
-  // path: /project/react-cli.html
-  "v-61413f94": defineAsyncComponent(() => import(/* webpackChunkName: "v-61413f94" */"D:/item/2025study/Vue/Webpack/courseware/webpack_docs/docs/.vuepress/.temp/pages/project/react-cli.html.vue")),
-  // path: /project/
-  "v-c9bbfba2": defineAsyncComponent(() => import(/* webpackChunkName: "v-c9bbfba2" */"D:/item/2025study/Vue/Webpack/courseware/webpack_docs/docs/.vuepress/.temp/pages/project/index.html.vue")),
-  // path: /project/summary.html
-  "v-2a04a4a2": defineAsyncComponent(() => import(/* webpackChunkName: "v-2a04a4a2" */"D:/item/2025study/Vue/Webpack/courseware/webpack_docs/docs/.vuepress/.temp/pages/project/summary.html.vue")),
-  // path: /project/vue-cli.html
-  "v-04c1b7cf": defineAsyncComponent(() => import(/* webpackChunkName: "v-04c1b7cf" */"D:/item/2025study/Vue/Webpack/courseware/webpack_docs/docs/.vuepress/.temp/pages/project/vue-cli.html.vue")),
+  "v-f9e30908": defineAsyncComponent(() => import(/* webpackChunkName: "v-f9e30908" */"D:/study/2025/Vue/Webpack/courseware/webpack_docs/docs/.vuepress/.temp/pages/intro/index.html.vue")),
   // path: /origin/loader.html
-  "v-1cfd2df6": defineAsyncComponent(() => import(/* webpackChunkName: "v-1cfd2df6" */"D:/item/2025study/Vue/Webpack/courseware/webpack_docs/docs/.vuepress/.temp/pages/origin/loader.html.vue")),
+  "v-1cfd2df6": defineAsyncComponent(() => import(/* webpackChunkName: "v-1cfd2df6" */"D:/study/2025/Vue/Webpack/courseware/webpack_docs/docs/.vuepress/.temp/pages/origin/loader.html.vue")),
   // path: /origin/plugin.html
-  "v-2d7562d6": defineAsyncComponent(() => import(/* webpackChunkName: "v-2d7562d6" */"D:/item/2025study/Vue/Webpack/courseware/webpack_docs/docs/.vuepress/.temp/pages/origin/plugin.html.vue")),
+  "v-2d7562d6": defineAsyncComponent(() => import(/* webpackChunkName: "v-2d7562d6" */"D:/study/2025/Vue/Webpack/courseware/webpack_docs/docs/.vuepress/.temp/pages/origin/plugin.html.vue")),
   // path: /origin/
-  "v-2259a8b0": defineAsyncComponent(() => import(/* webpackChunkName: "v-2259a8b0" */"D:/item/2025study/Vue/Webpack/courseware/webpack_docs/docs/.vuepress/.temp/pages/origin/index.html.vue")),
+  "v-2259a8b0": defineAsyncComponent(() => import(/* webpackChunkName: "v-2259a8b0" */"D:/study/2025/Vue/Webpack/courseware/webpack_docs/docs/.vuepress/.temp/pages/origin/index.html.vue")),
   // path: /origin/summary.html
-  "v-860cdcba": defineAsyncComponent(() => import(/* webpackChunkName: "v-860cdcba" */"D:/item/2025study/Vue/Webpack/courseware/webpack_docs/docs/.vuepress/.temp/pages/origin/summary.html.vue")),
+  "v-860cdcba": defineAsyncComponent(() => import(/* webpackChunkName: "v-860cdcba" */"D:/study/2025/Vue/Webpack/courseware/webpack_docs/docs/.vuepress/.temp/pages/origin/summary.html.vue")),
+  // path: /project/react-cli.html
+  "v-61413f94": defineAsyncComponent(() => import(/* webpackChunkName: "v-61413f94" */"D:/study/2025/Vue/Webpack/courseware/webpack_docs/docs/.vuepress/.temp/pages/project/react-cli.html.vue")),
+  // path: /project/
+  "v-c9bbfba2": defineAsyncComponent(() => import(/* webpackChunkName: "v-c9bbfba2" */"D:/study/2025/Vue/Webpack/courseware/webpack_docs/docs/.vuepress/.temp/pages/project/index.html.vue")),
+  // path: /project/summary.html
+  "v-2a04a4a2": defineAsyncComponent(() => import(/* webpackChunkName: "v-2a04a4a2" */"D:/study/2025/Vue/Webpack/courseware/webpack_docs/docs/.vuepress/.temp/pages/project/summary.html.vue")),
+  // path: /project/vue-cli.html
+  "v-04c1b7cf": defineAsyncComponent(() => import(/* webpackChunkName: "v-04c1b7cf" */"D:/study/2025/Vue/Webpack/courseware/webpack_docs/docs/.vuepress/.temp/pages/project/vue-cli.html.vue")),
+  // path: /base/base.html
+  "v-a6cc4bba": defineAsyncComponent(() => import(/* webpackChunkName: "v-a6cc4bba" */"D:/study/2025/Vue/Webpack/courseware/webpack_docs/docs/.vuepress/.temp/pages/base/base.html.vue")),
+  // path: /base/clean.html
+  "v-21f1ba15": defineAsyncComponent(() => import(/* webpackChunkName: "v-21f1ba15" */"D:/study/2025/Vue/Webpack/courseware/webpack_docs/docs/.vuepress/.temp/pages/base/clean.html.vue")),
+  // path: /base/config.html
+  "v-e703ecdc": defineAsyncComponent(() => import(/* webpackChunkName: "v-e703ecdc" */"D:/study/2025/Vue/Webpack/courseware/webpack_docs/docs/.vuepress/.temp/pages/base/config.html.vue")),
+  // path: /base/css.html
+  "v-80c5304a": defineAsyncComponent(() => import(/* webpackChunkName: "v-80c5304a" */"D:/study/2025/Vue/Webpack/courseware/webpack_docs/docs/.vuepress/.temp/pages/base/css.html.vue")),
+  // path: /base/development.html
+  "v-34029aa3": defineAsyncComponent(() => import(/* webpackChunkName: "v-34029aa3" */"D:/study/2025/Vue/Webpack/courseware/webpack_docs/docs/.vuepress/.temp/pages/base/development.html.vue")),
+  // path: /base/font.html
+  "v-85d4d936": defineAsyncComponent(() => import(/* webpackChunkName: "v-85d4d936" */"D:/study/2025/Vue/Webpack/courseware/webpack_docs/docs/.vuepress/.temp/pages/base/font.html.vue")),
+  // path: /base/html.html
+  "v-ab115cee": defineAsyncComponent(() => import(/* webpackChunkName: "v-ab115cee" */"D:/study/2025/Vue/Webpack/courseware/webpack_docs/docs/.vuepress/.temp/pages/base/html.html.vue")),
+  // path: /base/image.html
+  "v-03183b7a": defineAsyncComponent(() => import(/* webpackChunkName: "v-03183b7a" */"D:/study/2025/Vue/Webpack/courseware/webpack_docs/docs/.vuepress/.temp/pages/base/image.html.vue")),
+  // path: /base/javascript.html
+  "v-0bd200c7": defineAsyncComponent(() => import(/* webpackChunkName: "v-0bd200c7" */"D:/study/2025/Vue/Webpack/courseware/webpack_docs/docs/.vuepress/.temp/pages/base/javascript.html.vue")),
+  // path: /base/minifyHtml.html
+  "v-50d92e02": defineAsyncComponent(() => import(/* webpackChunkName: "v-50d92e02" */"D:/study/2025/Vue/Webpack/courseware/webpack_docs/docs/.vuepress/.temp/pages/base/minifyHtml.html.vue")),
+  // path: /base/optimizeCss.html
+  "v-65f7fcd8": defineAsyncComponent(() => import(/* webpackChunkName: "v-65f7fcd8" */"D:/study/2025/Vue/Webpack/courseware/webpack_docs/docs/.vuepress/.temp/pages/base/optimizeCss.html.vue")),
+  // path: /base/other.html
+  "v-499655ae": defineAsyncComponent(() => import(/* webpackChunkName: "v-499655ae" */"D:/study/2025/Vue/Webpack/courseware/webpack_docs/docs/.vuepress/.temp/pages/base/other.html.vue")),
+  // path: /base/output.html
+  "v-5a4f3333": defineAsyncComponent(() => import(/* webpackChunkName: "v-5a4f3333" */"D:/study/2025/Vue/Webpack/courseware/webpack_docs/docs/.vuepress/.temp/pages/base/output.html.vue")),
+  // path: /base/production.html
+  "v-6940ce0a": defineAsyncComponent(() => import(/* webpackChunkName: "v-6940ce0a" */"D:/study/2025/Vue/Webpack/courseware/webpack_docs/docs/.vuepress/.temp/pages/base/production.html.vue")),
+  // path: /base/
+  "v-1455d425": defineAsyncComponent(() => import(/* webpackChunkName: "v-1455d425" */"D:/study/2025/Vue/Webpack/courseware/webpack_docs/docs/.vuepress/.temp/pages/base/index.html.vue")),
+  // path: /base/server.html
+  "v-5e3bb631": defineAsyncComponent(() => import(/* webpackChunkName: "v-5e3bb631" */"D:/study/2025/Vue/Webpack/courseware/webpack_docs/docs/.vuepress/.temp/pages/base/server.html.vue")),
+  // path: /base/summary.html
+  "v-ce2194d0": defineAsyncComponent(() => import(/* webpackChunkName: "v-ce2194d0" */"D:/study/2025/Vue/Webpack/courseware/webpack_docs/docs/.vuepress/.temp/pages/base/summary.html.vue")),
   // path: /senior/enhanceExperience.html
-  "v-7964f787": defineAsyncComponent(() => import(/* webpackChunkName: "v-7964f787" */"D:/item/2025study/Vue/Webpack/courseware/webpack_docs/docs/.vuepress/.temp/pages/senior/enhanceExperience.html.vue")),
+  "v-7964f787": defineAsyncComponent(() => import(/* webpackChunkName: "v-7964f787" */"D:/study/2025/Vue/Webpack/courseware/webpack_docs/docs/.vuepress/.temp/pages/senior/enhanceExperience.html.vue")),
   // path: /senior/liftingSpeed.html
-  "v-7b858f23": defineAsyncComponent(() => import(/* webpackChunkName: "v-7b858f23" */"D:/item/2025study/Vue/Webpack/courseware/webpack_docs/docs/.vuepress/.temp/pages/senior/liftingSpeed.html.vue")),
+  "v-7b858f23": defineAsyncComponent(() => import(/* webpackChunkName: "v-7b858f23" */"D:/study/2025/Vue/Webpack/courseware/webpack_docs/docs/.vuepress/.temp/pages/senior/liftingSpeed.html.vue")),
   // path: /senior/optimizePerformance.html
-  "v-5489448c": defineAsyncComponent(() => import(/* webpackChunkName: "v-5489448c" */"D:/item/2025study/Vue/Webpack/courseware/webpack_docs/docs/.vuepress/.temp/pages/senior/optimizePerformance.html.vue")),
+  "v-5489448c": defineAsyncComponent(() => import(/* webpackChunkName: "v-5489448c" */"D:/study/2025/Vue/Webpack/courseware/webpack_docs/docs/.vuepress/.temp/pages/senior/optimizePerformance.html.vue")),
   // path: /senior/
-  "v-3fe9ea34": defineAsyncComponent(() => import(/* webpackChunkName: "v-3fe9ea34" */"D:/item/2025study/Vue/Webpack/courseware/webpack_docs/docs/.vuepress/.temp/pages/senior/index.html.vue")),
+  "v-3fe9ea34": defineAsyncComponent(() => import(/* webpackChunkName: "v-3fe9ea34" */"D:/study/2025/Vue/Webpack/courseware/webpack_docs/docs/.vuepress/.temp/pages/senior/index.html.vue")),
   // path: /senior/reduceVolume.html
-  "v-5460e2da": defineAsyncComponent(() => import(/* webpackChunkName: "v-5460e2da" */"D:/item/2025study/Vue/Webpack/courseware/webpack_docs/docs/.vuepress/.temp/pages/senior/reduceVolume.html.vue")),
+  "v-5460e2da": defineAsyncComponent(() => import(/* webpackChunkName: "v-5460e2da" */"D:/study/2025/Vue/Webpack/courseware/webpack_docs/docs/.vuepress/.temp/pages/senior/reduceVolume.html.vue")),
   // path: /senior/summary.html
-  "v-1d695a4e": defineAsyncComponent(() => import(/* webpackChunkName: "v-1d695a4e" */"D:/item/2025study/Vue/Webpack/courseware/webpack_docs/docs/.vuepress/.temp/pages/senior/summary.html.vue")),
+  "v-1d695a4e": defineAsyncComponent(() => import(/* webpackChunkName: "v-1d695a4e" */"D:/study/2025/Vue/Webpack/courseware/webpack_docs/docs/.vuepress/.temp/pages/senior/summary.html.vue")),
   // path: /404.html
-  "v-3706649a": defineAsyncComponent(() => import(/* webpackChunkName: "v-3706649a" */"D:/item/2025study/Vue/Webpack/courseware/webpack_docs/docs/.vuepress/.temp/pages/404.html.vue")),
+  "v-3706649a": defineAsyncComponent(() => import(/* webpackChunkName: "v-3706649a" */"D:/study/2025/Vue/Webpack/courseware/webpack_docs/docs/.vuepress/.temp/pages/404.html.vue")),
 }
